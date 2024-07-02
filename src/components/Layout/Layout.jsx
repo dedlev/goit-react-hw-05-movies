5b4ae2c5add84d89ea40fbe08d6a327e75102d9a
dedlev <150597998+dedlev@users.noreply.github.com>
2024-07-02 13:27:13 +0300

    adds code splitting
    adds code splitting

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from 'react'
+import React, { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Container, Header, Link } from './Layoutstyled'
 import { Loader } from 'ui/Loader';
